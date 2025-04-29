b2c50795eb07b056bc13089d6e639cb4375ee52c
Daniel <danielmorozov58@gmail.com>
2025-04-29 11:21:33 -0700

    Updated reflectingLat

--- reflectingLat.py
+++ reflectingLat.py
@@ -1,1 +1,9 @@
+import numpy as np
+
 def reflectingLat(lat):
+    """
+    Function to accept a grid and to return a grid extended one cell in each direction with reflecting boundary conditions
+    """
+    latNS = np.vstack((lat[0, :], lat, lat[-1, :]))
+    latR = np.hstack((latNS[:, 0:1], latNS, latNS[:, -1:]))
+    return latR
