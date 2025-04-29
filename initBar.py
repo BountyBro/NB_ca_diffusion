434612adaa8caac704b836ed9cd67b5174571f22
Alyssa Sandoval <alyssasandoval@MacBook-Air-181.local>
2025-04-29 11:35:48 -0700

    initBar function

--- initBar.py
+++ initBar.py
@@ -1,1 +1,25 @@
+import numpy as np
 
+AMBIENT = 25
+HOT = 50
+COLD = 0
+
+def initBar(m, n, hotSites, coldSites):
+    """
+    initializes a bar with ambient temperature
+    """
+    ambientBar = np.full((m, n), AMBIENT)
+    return applyHotCold(ambientBar, hotSites, coldSites)
+
+def applyHotCold(bar, hotSites, coldSites):
+    """
+    Assigns hot and cold temperatures to the bar
+    """
+    m, n = bar.shape
+    for i in range(m):
+        for j in range(n):
+            if (i, j) in hotSites:
+                bar[i, j] = HOT
+            elif (i, j) in coldSites:
+                bar[i, j] = COLD
+    return bar
