4b500392bf2d8dfda2f0fd3b09bc9b7a1935369d
BountyBro <32472955+BountyBro@users.noreply.github.com>
2025-04-29 12:10:58 -0700

    Update applyDiffusionExtended.py

--- applyDiffusionExtended.py
+++ applyDiffusionExtended.py
@@ -1,2 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
+
+def applyDefusionExtended(diff_r, latExt):
+  toReturn = np.full(shape= latExt.shape, fill_value= (255, 0, 0))
+  rows, cols = tuple(np.subtract(latExt.shape, (2, 2)))  # Uses numpy tuple unpacking and subtraction to define true rows and cols.
+  for row in range(1, rows + 2):
+    for col in range(1, cols + 2):
+        toReturn[row, col] = tuple( 5.1 * diffusion(diff_r, latExt[row, col], latExt[row - 1, col], latExt[row - 1, col + 1], latExt[row, col + 1], latExt[row + 1, col + 1], latExt[row + 1, col], latExt[row + 1, col - 1], latExt[row - 1, col], latExt[row - 1, col - 1]) for i in range(3))
+  return toReturn      
